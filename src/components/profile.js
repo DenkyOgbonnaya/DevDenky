e4b4678e18a4e8d2b22d842d03f0532e66c07143
DenkyOgbonnaya <denkyogb@gmail.com>
2019-08-15 23:19:55 +0100

    revamp profile ui

--- src/components/profile.js
+++ src/components/profile.js
@@ -1,17 +1,31 @@
 import React from 'react';
-
-const name = '<Dennis Ogbonnaya />'
+import profilepix from './denky.JPG';
+ 
 const Profile = (props) =>
     <div className= 'profile'>
-            <p className='headline'> 
-                <span>  {name}  </span> <br />
-                Web Developer
-            </p>
-        <p>I am an experienced Web Developer skilled in building dynamic, scalable and responsive websites/web apps, using
-            modern day JavaScript, FrameWorks and Libraries.
-        If you are a business seeking a web presence to reach more audience and increase sales, or an 
-        Employer looking to hire, you can <a href='/ff' onClick={(e)=> {props.scrollToContact(e)} }>Contact me </a>.</p>
+        <img src={profilepix} alt='profile pix' className='avatar' />
+        <div className='name'> Dennis Ogbonnaya </div>
+        <div className='title'> <img src= {require('../images/icons/web18.png')} alt='title'/> Web Developer </div>
+        <p className='description'> 
+        I am a software developer focused on web, I build dynamic, scalable and 
+        responsive web application softWare using modern day JavaScript, Frameworks 
+        and Libraries. If you are a business seeking a web presence to reach more 
+        audience or an Employer looking to hire, you can <a href='' onClick= {e => {props.scrollToContact(e)}}> contact me </a> .
+         </p>
+        <span className='socials' >
+            <ul> 
+                <li>
+                    <a href ='https://github.com/DenkyOgbonnaya' >  <img src= {require('../images/icons/github.png')} alt='github'/> </a>
+                </li>
+                <li> 
+                    <a href ='https://linkedin.com/in/dennis-ogbonnaya' > <img src= {require('../images/icons/linked18.png')} alt='linkedin'/> </a>
+                </li>
+                <li>
+                    <a href ='https://twitter.com/DenkyOgbonnaya' > <img src= {require('../images/icons/twit18.png')} alt='twitter'/> </a>
+                </li>
+            </ul>
         
+          </span>
     </div>
 
 export default Profile;
