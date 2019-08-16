455c380e65e34a00c27bd6411603f093d6054df0
DenkyOgbonnaya <denkyogb@gmail.com>
2019-08-17 00:36:55 +0100

    modified

--- src/components/profile.js
+++ src/components/profile.js
@@ -8,7 +8,7 @@ const Profile = (props) =>
         <div className='title'> <img src= {require('../images/icons/web18.png')} alt='title'/> Web Developer </div>
         <p className='description'> 
         I am a software developer focused on web, I build dynamic, scalable and 
-        responsive web application softWare using modern day JavaScript, Frameworks 
+        responsive web application software using modern day JavaScript, Frameworks 
         and Libraries. If you are a business seeking a web presence to reach more 
         audience or an Employer looking to hire, you can <a href='' onClick= {e => {props.scrollToContact(e)}}> contact me </a> .
          </p>
