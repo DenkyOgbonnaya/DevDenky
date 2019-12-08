1a52fa22fb04702871dbfacbdb2f56901c148add
DenkyOgbonnaya <denkyogb@gmail.com>
2019-12-08 14:34:23 -0800

    Updates

---
+++ precache-manifest.87d2dd80fab1ceafd54020f846bc220b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "58b357f853d6bb24aa5c49b8c3ce324c",
+    "url": "/DevDenky/static/media/denky.58b357f8.JPG"
+  },
+  {
+    "revision": "32727f8800c35a13a9f7",
+    "url": "/DevDenky/static/js/runtime~main.32727f88.js"
+  },
+  {
+    "revision": "5ddb37d42027b0f30d12",
+    "url": "/DevDenky/static/js/main.5ddb37d4.chunk.js"
+  },
+  {
+    "revision": "aa0289ec45c205bbbb27",
+    "url": "/DevDenky/static/js/1.aa0289ec.chunk.js"
+  },
+  {
+    "revision": "5ddb37d42027b0f30d12",
+    "url": "/DevDenky/static/css/main.53ade996.chunk.css"
+  },
+  {
+    "revision": "aa0289ec45c205bbbb27",
+    "url": "/DevDenky/static/css/1.e926e3c7.chunk.css"
+  },
+  {
+    "revision": "8f0f5cdbd3b2c1af1041078ba15ad959",
+    "url": "/DevDenky/index.html"
+  }
+];
